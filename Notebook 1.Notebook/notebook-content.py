70b81887b48043054e3c34eeb846d3c385ace2e3
Benmcc74 <Benmccarthy74@hotmail.com>
2026-05-16 15:57:37 +0100

    Committing 1 item from workspace 7b94f468-80f9-400a-a564-dfaacf1dab5f

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "a2c3ae2e-6fd2-41b6-bde4-1529a8103742",
+# META       "default_lakehouse_name": "LHDEVGOLDSTAGING",
+# META       "default_lakehouse_workspace_id": "7b94f468-80f9-400a-a564-dfaacf1dab5f",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "a2c3ae2e-6fd2-41b6-bde4-1529a8103742"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+import time
+print('Before...')
+time.sleep(3)
+print('After...')
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
